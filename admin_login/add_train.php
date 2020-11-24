b9f027ac7f6c09f433819db4c5e87bd381016dda
grtcoder <divyanshumathpal@gmail.com>
2020-11-24 20:25:36 +0530

    frontend added

--- admin_login/add_train.php
+++ admin_login/add_train.php
@@ -1,68 +1,160 @@
 <?php
    include('session.php');
 ?>
-<html>
-
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
 <head>
-  <title>Welcome </title>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+  <style>
+    html, body {
+    display: flex;
+    justify-content: center;
+    font-family: Roboto, Arial, sans-serif;
+    font-size: 15px;
+    }
+    form {
+    border: 5px solid #f1f1f1;
+    margin-top: 60px;
+    left: 50%;
+    right:50%;
+    position: absolute;
+    }
+    input[type=text], input[type=password] {
+    width: 100%;
+    padding: 16px 8px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    }
+    button {
+    background-color: #8ebf42;
+    color: white;
+    padding: 14px 0;
+    margin: 10px 0;
+    border: none;
+    cursor: grabbing;
+    width: 100%;
+    }
+    h1 {
+    text-align:center;
+    fone-size:18;
+    }
+    button:hover {
+    opacity: 0.8;
+    }
+    .formcontainer {
+    text-align: left;
+    margin: 24px 50px 12px;
+    }
+    .container {
+    padding: 16px 0;
+    text-align:left;
+    }
+    span.psw {
+    float: right;
+    padding-top: 0;
+    padding-right: 15px;
+    }
+    /* Change styles for span on extra small screens */
+    @media screen and (max-width: 300px) {
+    span.psw {
+    display: block;
+    float: none;
+    }
+  </style>
 </head>
 <body>
-  <h1>Welcome <?php echo $_SESSION['admin_id']; ?></h1>
-<form class="" action="" method="post">
-    <label for="train_no">train_no</label><br>
-    <input type="text" id="train_no" name="train_no"><br>
-    <label for="source_id">source_id</label><br>
-    <input type="text" id="source_id" name="source_id"><br>
-    <label for="destination_id">destination_id</label><br>
-    <input type="text" id="destination_id" name="destination_id"><br>
-    <label for="coaches_capacity">coaches_capacity</label><br>
-    <input type="text" id="coaches_capacity" name="coaches_capacity"><br>
-    <input type="submit" value="Submit" onclick="clear()">
-    <script type="text/javascript">
-      function clear(){
-        document.getElementById('train_no')='';
-        document.getElementById('source_id')='';
-        document.getElementById('destination_id')='';
-        document.getElementById('coaches_capacity')='';
-      }
-    </script>
-  </form>
-  <button type="button" name="button" onclick="window.history.back()">Back</button>
-  <h2><a href="logout.php">Sign Out</a></h2>
-  <?php
-  if($_SERVER["REQUEST_METHOD"] == "POST") {
-    if($_POST['train_no']=='' or $_POST['source_id']=='' or $_POST['destination_id']=='' or $_POST['coaches_capacity']==''){
-      echo '<script>alert("No field should be empty")</script>';
-      die();
-    }
-    if(strlen($_POST['train_no'])!=5 and is_numeric($_POST['train_no'])){
-      echo '<script>alert("train_no should have length 5")</script>';
-      die();
-    }
-    if(strlen($_POST['source_id'])!=5 and is_numeric($_POST['source_id'])){
-      echo '<script>alert("source_id should have length 5")</script>';
-      die();
-    }
-    if(strlen($_POST['destination_id'])!=5 and is_numeric($_POST['destination_id'])){
-      echo '<script>alert("destination_id should have length 5")</script>';
-      die();
-    }
-    $sql = "SELECT * FROM train WHERE train_no = '{$_POST['train_no']}'";
-    $result = pg_num_rows(pg_query($db,$sql));
-    if ($result!=0) {
-      echo '<script>alert("train_no already exists")</script>';
-      die();
-    }
-    if($_POST['source_id']==$_POST['destination_id']){
-      echo '<script>alert("Source and Destination id cannot be same")</script>';
-      die();
-    }
-      $result = $result = pg_query_params('CALL Insert_Train($1, $2, $3, $4)',
-                array($_POST['train_no'],$_POST['source_id'],$_POST['destination_id'],$_POST['coaches_capacity']))
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+  <!-- Brand/logo -->
+  <!-- <a class="navbar-brand" href="#">Railway Reservation system</a> -->
 
-              or die('Unable to CALL stored procedure: ' . pg_last_error());
-      echo 'train_added successfully.';
+  <!-- Links -->
+  <h2 class="navbar-brand">Welcome, <?php echo $_SESSION['admin_id']; ?></h2>
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="./add_train.php">Add Train</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="'./release_train.php">Release Train</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="logout.php">Logout</a>
+    </li>
+  </ul>
+</nav>
+<form method="post">
+  <h1>Admin Login</h1>
+  <div class="formcontainer">
+  <div class="container">
+  <span style="padding-bottom:2000px"></span>
+    <label for="train_no"><strong>train_no</strong></label>
+    <input type="text" placeholder="train_no" name="train_no" required>
+    <label for="source_id"><strong>source_id</strong></label>
+    <input type="text" placeholder="source_id" name="source_id" required>
+    <label for="destination_id"><strong>destination_id</strong></label>
+    <input type="text" placeholder="destination_id" name="destination_id" required>
+    <label for="coaches_capacity"><strong>coaches_capacity</strong></label>
+    <input type="text" placeholder="coaches_capacity" name="coaches_capacity" required>
+    <div style = "font-size:14px; color:#cc0000; margin-top:10px"></div>
+  </div>
+  <button type="submit" value="submit" onclick="clear()">Login</button>
+  <script type="text/javascript">
+    function clear(){
+      document.getElementById('train_no')='';
+      document.getElementById('source_id')='';
+      document.getElementById('destination_id')='';
+      document.getElementById('coaches_capacity')='';
+    }
+  </script>
+<button onclick="goBack()">Go Back</button>
+</div>
+<script>
+function goBack() {
+  window.history.back();
+}
+</script>
+</form>
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  if($_POST['train_no']=='' or $_POST['source_id']=='' or $_POST['destination_id']=='' or $_POST['coaches_capacity']==''){
+    echo '<script>alert("No field should be empty")</script>';
+    die();
   }
-  ?>
+  if(strlen($_POST['train_no'])!=5 and is_numeric($_POST['train_no'])){
+    echo '<script>alert("train_no should have length 5")</script>';
+    die();
+  }
+  if(strlen($_POST['source_id'])!=5 and is_numeric($_POST['source_id'])){
+    echo '<script>alert("source_id should have length 5")</script>';
+    die();
+  }
+  if(strlen($_POST['destination_id'])!=5 and is_numeric($_POST['destination_id'])){
+    echo '<script>alert("destination_id should have length 5")</script>';
+    die();
+  }
+  $sql = "SELECT * FROM train WHERE train_no = '{$_POST['train_no']}'";
+  $result = pg_num_rows(pg_query($db,$sql));
+  if ($result!=0) {
+    echo '<script>alert("train_no already exists")</script>';
+    die();
+  }
+  if($_POST['source_id']==$_POST['destination_id']){
+    echo '<script>alert("Source and Destination id cannot be same")</script>';
+    die();
+  }
+    $result = $result = pg_query_params('CALL Insert_Train($1, $2, $3, $4)',
+              array($_POST['train_no'],$_POST['source_id'],$_POST['destination_id'],$_POST['coaches_capacity']))
+
+            or die('Unable to CALL stored procedure: ' . pg_last_error());
+    echo 'train_added successfully.';
+}
+?>
 </body>
 </html>
--- admin_login/welcome.php
+++ admin_login/welcome.php
@@ -1,7 +1,7 @@
 <?php
    include('session.php');
 ?>
-<html>
+<!-- <html>
 
    <head>
       <title>Welcome </title>
@@ -13,5 +13,39 @@
       <button type="button" onclick="window.location.href = './release_train.php';">Release Train</button>
       <h2><a href = "logout.php">Sign Out</a></h2>
    </body>
+
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <!-- Brand/logo -->
+    <!-- <a class="navbar-brand" href="#">Railway Reservation system</a> -->
+
+    <!-- Links -->
+    <h2 class="navbar-brand">Welcome, <?php echo $_SESSION['admin_id']; ?></h2>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="./add_train.php">Add Train</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./release_train.php">Release Train</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li>
+    </ul>
+  </nav>
+
+</body>
 
 </html>
